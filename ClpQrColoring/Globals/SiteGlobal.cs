4f7a59e4e8bb2e6e1700a5c2f1a5d45090f05a6c
szewa-polyu <szewa.wong@connect.polyu.hk>
2018-11-06 16:49:26 +0800

    updated ColourSheet

--- ClpQrColoring/Globals/SiteGlobal.cs
+++ ClpQrColoring/Globals/SiteGlobal.cs
@@ -1,5 +1,6 @@
 ﻿using FileHelperDLL;
 using System;
+using System.IO;
 using System.Web;
 using System.Web.Configuration;
 
@@ -140,7 +141,7 @@ namespace ClpQrColoring.Globals
 
         private static string GetFullPath(string path)
         {
-            return FileHelper.IsAbsolutePath(path) ?
+            return Path.IsPathRooted(path) ?
                 path : HttpContext.Current.Server.MapPath(path);
         }
 
