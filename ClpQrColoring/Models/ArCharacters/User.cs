3bafbf97a41f213f4a265333b79893447e850e2f
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2020-02-18 12:45:59 +0800

    Update User.cs

--- ClpQrColoring/Models/ArCharacters/User.cs
+++ ClpQrColoring/Models/ArCharacters/User.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.ComponentModel.DataAnnotations;
-using ClpQrColoring.DataAccess;
+﻿using ClpQrColoring.DataAccess;
 using ClpQrColoring.Validations;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
 
 namespace ClpQrColoring.Models.ArCharacters
 {
